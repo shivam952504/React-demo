7f9a4a3b7f54f4460e3a52eb0b519e31d8bff7e0
shivam952504 <123369662+shivam952504@users.noreply.github.com>
2026-02-09 15:56:35 +0530

    Delete src/Captilize.jsx

--- src/Captilize.jsx
+++
@@ -1,8 +0,0 @@
-export const toTitleCase = (str = "") =>
-  str
-    .toLowerCase()
-    .split(" ")
-    .filter(Boolean)
-    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(" ");
-<h2>{toTitleCase(clientName)}</h2>
